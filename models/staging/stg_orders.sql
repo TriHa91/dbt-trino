ae3a08013d4e9a9d38a89117f514610b4a6fb408
Your Name <your-email@example.com>
2025-04-01 07:28:54 +0000

    add stg order model

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized='table'
+) }}
+
+with source as (
+
+    select * from {{ source('source_tbl2', 'raw_orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from source
+
+)
+
+select * from renamed
